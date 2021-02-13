1481fc8287b91a00bcddabff9c14aaed7ef3b57a
Robert Wilbrandt <robert@stamm-wilbrandt.de>
2021-02-13 21:50:09 +0100

    Fix clock frequency

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <zephyr.h>
 
+void print_msg(struct k_timer* timer);
+
 void main(void)
 {
-  printk("Hello World!");
+  struct k_timer timer;
+  k_timer_init(&timer, print_msg, NULL);
+
+  k_timer_start(&timer, K_SECONDS(1), K_SECONDS(1));
+  while (1) {}
+}
+
+void print_msg(struct k_timer* timer)
+{
+  printk("Hello World!\n");
 }
